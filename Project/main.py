36fa79def0b3dc469a96eabc408df370243a053f
Nícolas Maisonnette <nicolasmd9@hotmail.com>
2021-05-27 12:41:34 -0300

    update main 😅😅
    excel, loading screen

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,13 @@
 import imutils
 import cv2
 import numpy as np
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+from PIL import Image
+def get_date_taken(path):
+    return Image.open(path)._getexif()[36867]
 
 l_xStart = -1
 l_yStart = -1
@@ -11,18 +18,42 @@ l_width = -1
 contador = 0
 l_valMax = -1
 c_valMax = -1
-imagensTemplate = [r'C:\Users\nicol\Documents\GitHub\OpenCV_Vehicle_Detection\Project\images\carUnicoJojo1.png', 
-                   r'C:\Users\nicol\Documents\GitHub\OpenCV_Vehicle_Detection\Project\images\carUnicoJojo2.png',
-                   r'C:\Users\nicol\Documents\GitHub\OpenCV_Vehicle_Detection\Project\images\carUnicoJojo3.png',
-                   r'C:\Users\nicol\Documents\GitHub\OpenCV_Vehicle_Detection\Project\images\carUnicoJojo4.png',
-                   r'C:\Users\nicol\Documents\GitHub\OpenCV_Vehicle_Detection\Project\images\carUnicoJojo5.png']
-# t1 - vals_maxs = [41602273, 48509457, 66520257, 39139037, 46415409]
-vals_maxs = [42961905, 57359841, 57067197, 1, 1]
+valMax_last = -1
+imagensTemplate = [r'.\Project\images#\carUnicoJojo1.png', 
+                   r'.\Project\images#\carUnicoJojo2.png',
+                   r'.\Project\images#\carUnicoJojo3.png',
+                   r'.\Project\images#\carUnicoJojo4.png',
+                   r'.\Project\images#\carUnicoJojo5.png']
+                   
+vals_maxs = [41602273, 48509457, 66520257, 39139037, 46415409]
+#vals_maxs = [42961905, 57359841, 57067197, 1, 1]
 
-#open the main image and convert it to gray scale image
-main_image = cv2.imread(r'C:\Users\nicol\Documents\GitHub\OpenCV_Vehicle_Detection\Project\images\carJojo2.jpg')
+Tk().withdraw()
+filename = askopenfilename()
+
+main_image = cv2.imread(filename)
 (m_height, m_width) = main_image.shape[:2]
 
+cv2.imshow('Aguarde', cv2.imread(r'.\Project\images#\carregando.png'))
+cv2.waitKey(0)
+
+try:
+   workbook = load_workbook("./Project/Resultados.xlsx")
+   sheet = workbook.active
+except FileNotFoundError:
+   workbook = Workbook()
+   sheet = workbook.active
+   sheet["A1"].font = Font(bold=True)
+   sheet["B1"].font = Font(bold=True)
+   sheet["C1"].font = Font(bold=True)
+   sheet["A1"] = "Data (y:m:d h:min:s)"
+   sheet["B1"] = "Quantidade"
+   sheet["C1"] = "Nome do Arquivo"
+   sheet.column_dimensions["A"].width = 30
+   sheet.column_dimensions["B"].width = 15
+   sheet.column_dimensions["C"].width = 120
+
+   data = get_date_taken(filename)
 i = 0
 while(i < len(imagensTemplate)):
    #Open template and get canny
@@ -54,10 +85,17 @@ while(i < len(imagensTemplate)):
       
       print(f'c_valMax: {c_valMax}') #VALMAX É A COMPATIBILIDADE COM O TEMPLATE
       if l_valMax >= 0:
-         """l_valMax"""
-         if c_valMax < vals_maxs[i]: # 14100000        cvalmax do match q começou a dar erro      41602177|46261429
-            print(f'Quantidade: {contador}')
+         if c_valMax == valMax_last:
+            print(f'Quantidade {contador}')
+            contador = contador - 1
+            valMax_last = -1
             break
+
+         valMax_last = c_valMax
+
+         #if c_valMax < vals_maxs[i]:
+            #print(f'Quantidade: {contador}')
+            #break
 
       l_valMax = c_valMax
       contador = contador + 1
@@ -81,9 +119,19 @@ while(i < len(imagensTemplate)):
       main_image = cv2.ellipse(main_image, (x_coordinate, y_coordinate), (round((x_end - x_start)/1.7), round((y_end - y_start)/1.7)), 0.0, 0.0, 360.0, (255, 0, 0), -1)
       
       main_image_resized = imutils.resize(main_image, width = int(500))
-      cv2.imshow('Template Found', main_image_resized)
-      cv2.waitKey(0)
+      #cv2.imshow('Template Found', main_image_resized)
+      #cv2.waitKey(0)
       
    i = i + 1
+
+arquivo = filename['#'+2:len(filename)]
+sheet.append([data, contador, arquivo])
+
+workbook.save(r".\Project\Resultados.xlsx")
+
+cv2.destroyAllWindows
 cv2.waitKey(0)
+cv2.imshow('Template Found', main_image_resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows
 
