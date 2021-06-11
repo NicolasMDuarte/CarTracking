81f6f2561353e995be24db2aa51aa2f5a0121249
Nícolas Duarte <nicolasmd9@hotmail.com>
2021-06-11 12:01:45 -0300

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,48 +1,77 @@
+#  MONITORAMENTO URBANO PARA AVALIAÇÃO DE MOBILIDADE URBANA COM 
+#        USO DE DRONE   E INTELIGÊNCIA ARTIFICIAL
+# -----------------------------------------------------------------
+# Programa para multi-detecção e contagem de objetos em uma imagem
+# -----------------------------------------------------------------
+# PIBIC-EM 2020/2021
+# Autores: Giovanna Pavani Martelli e Nícolas Maisonnette Duarte 
+
+
+# Importando bibliotecas do projeto
 import imutils
-import cv2
+import cv2  # OpenCV
 import numpy as np
-from openpyxl import load_workbook, Workbook
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from openpyxl import load_workbook, Workbook  # Excel
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font  # Styles do excel
+from tkinter import Tk  # Arquivos
 from tkinter.filedialog import askopenfilename
-from PIL import Image
+from PIL import Image # Imagem
+
+# Retorna a data de captura da imagem do caminho passado por parâmetro
 def get_date_taken(path):
     return Image.open(path)._getexif()[36867]
 
+# Declaração de variáveis globais
 data = ''
+# Pixels de começo e fim das imagens templates
 l_xStart = -1
 l_yStart = -1
 l_xEnd = -1
 l_yEnd = -1
+# Tamanhos da imagens
 l_height = -1
 l_width = -1
+# Contador de matches
 contador = 0
 l_valMax = -1
 c_valMax = -1
 valMax_last = -1
+# Vetor de imagens template 
 imagensTemplate = [r'.\Project\images#\carUnicoJojo1.png', 
                    r'.\Project\images#\carUnicoJojo2.png',
                    r'.\Project\images#\carUnicoJojo4.png',
-                   r'.\Project\images#\carUnicoJojo5.png']
+                   r'.\Project\images#\carUnicoJojo5.png',]
+                  #  r'.\Project\images#\carUnicoJojo7.png',
+                  #  r'.\Project\images#\carUnicoJojo8.png',
+                  #  r'.\Project\images#\carUnicoJojo9.png',
+                  #  r'.\Project\images#\carUnicoJojo10.png',
+                  #  r'.\Project\images#\carUnicoJojo11.png',
+                  #  r'.\Project\images#\carUnicoJojo12.png',
                    
-vals_maxs = [41602273, 48509457, 66520257, 39139037, 46415409]
+#vals_maxs = [41602273, 48509457, 66520257, 39139037, 46415409]
 #vals_maxs = [42961905, 57359841, 57067197, 1, 1]
 
+# Abre arquivos e imagens
 Tk().withdraw()
 filename = askopenfilename()
 
 main_image = cv2.imread(filename)
 (m_height, m_width) = main_image.shape[:2]
+# main_image = cv2.GaussianBlur(main_image, (15, 15), 2) (Tentamos blur, sem sucesso)
 
+# Tela de carregamento
 cv2.imshow('Aguarde', cv2.imread(r'.\Project\images#\carregando.png'))
 cv2.waitKey(0)
 
 try:
+   # Verifica se já existe um arquivo Excel
    workbook = load_workbook("./Project/Resultados.xlsx")
    sheet = workbook.active
 except FileNotFoundError:
+   # Caso não exista, cria-se um Resultados.xlsx
    workbook = Workbook()
    sheet = workbook.active
+   # Setta os headers da tabela
    sheet["A1"].font = Font(bold=True)
    sheet["B1"].font = Font(bold=True)
    sheet["C1"].font = Font(bold=True)
@@ -56,37 +85,43 @@ except FileNotFoundError:
 data = get_date_taken(filename)
 i = 0
 
+# Percorre o vetor de templates
 while(i < len(imagensTemplate)):
-   #Open template and get canny
+   # Abre o template e armazena o canny
    template = cv2.imread(imagensTemplate[i])
    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
    template = cv2.Canny(template, 10, 25)
    (height, width) = template.shape[:2]
    
-   while True:   
+   while True: 
+      # gray_imagem = main_image em escala de cinza  
       gray_image = cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
+      gray_exib = imutils.resize(gray_image, width = int(500))
+      cv2.imshow('Gray', gray_exib)
       temp_found = None
 
       for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-         #resize the image and store the ratio
+         # Redimensiona a gray_image e armazena o ratio
          resized_img = imutils.resize(gray_image, width = int(gray_image.shape[1] * scale))
          ratio = gray_image.shape[1] / float(resized_img.shape[1])
          if resized_img.shape[0] < height or resized_img.shape[1] < width:
             break
          
-         #Convert to edged image for checking
+         # Converte em edged image para verificar
          e = cv2.Canny(resized_img, 10, 25)
          match = cv2.matchTemplate(e, template, cv2.TM_CCOEFF)
          (_, val_max, _, loc_max) = cv2.minMaxLoc(match)
 
-
+         # Achou template válido
          if temp_found is None or val_max>temp_found[0]:
             temp_found = (val_max, loc_max, ratio)
             c_valMax = val_max
       
-      print(f'c_valMax: {c_valMax}') #VALMAX É A COMPATIBILIDADE COM O TEMPLATE
-      if l_valMax >= 0:
-         if c_valMax == valMax_last:
+      # ValMax representa, numericamente, a compatibilidade do template com a imagem
+      print(f'c_valMax: {c_valMax}') 
+      
+      if l_valMax >= 0: # Se não é a primeira vez
+         if c_valMax == valMax_last: # Verifica repetição do val_max
             contador = contador - 1
             print(f'Quantidade {contador}')
             valMax_last = -1
@@ -101,7 +136,7 @@ while(i < len(imagensTemplate)):
       l_valMax = c_valMax
       contador = contador + 1
 
-      #Get information from temp_found to compute x,y coordinate
+      # Pega a informação do temp_found e armazena as coordenadas X e Y
       (_, loc_max, r) = temp_found
       (x_start, y_start) = (int(loc_max[0]), int(loc_max[1]))
       (x_end, y_end) = (int((loc_max[0] + width)), int((loc_max[1] + height)))
@@ -111,14 +146,14 @@ while(i < len(imagensTemplate)):
       l_xEnd = x_end
       l_yEnd = y_end
 
-      #Draw elipse around the template
+      # Desenha uma elipse onde o teplate foi encontrado na main_image
       x_coordinate = (x_end-x_start)/2 + x_start
       y_coordinate = (y_end-y_start)/2 + y_start
       x_coordinate = round(x_coordinate)
       y_coordinate = round(y_coordinate)
 
       main_image = cv2.ellipse(main_image, (x_coordinate, y_coordinate), (round((x_end - x_start)/1.7), round((y_end - y_start)/1.7)), 0.0, 0.0, 360.0, (255, 0, 0), -1)
-      
+      # Redimensiona a main_image para que ela caiba na tela, já que sua resolução original é muito alta
       main_image_resized = imutils.resize(main_image, width = int(500))
       
       #testa uma por uma
@@ -129,12 +164,15 @@ while(i < len(imagensTemplate)):
 
 local = filename.find('#')
 arquivo = filename[local+2:len(filename)]
+# Registra resultado no arquivo Excel
 sheet.append([data, contador, arquivo])
-
+# Salva
 workbook.save(r".\Project\Resultados.xlsx")
 
+# Fecha todas as telas anteriores 
 cv2.destroyAllWindows
 cv2.waitKey(0)
+# Exibe a imagem com as elipses dos templates encontrados
 cv2.imshow('Template Found', main_image_resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows
